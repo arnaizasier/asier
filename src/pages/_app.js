b14e8e5cd4771dd4403b2eba5f50c4bf8bac3207
arnaizasier <arnaizasier14@gmail.com>
2025-06-03 14:34:16 +0100

    Crear _app.js

---
+++ src/pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/FAQ.module.css'; // Import global styles if needed
+import '/public/assets/css/marca.css'; // Your existing CSS file
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
